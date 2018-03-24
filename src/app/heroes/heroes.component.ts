64424dd9eb46bcf2529daeb14551b107792330ea
Gregg Wartman <gregg.wartman@ontario.ca>
2018-03-24 13:57:35 -0400

    Addinged Routing and some more pages

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,11 +11,9 @@ export class HeroesComponent implements OnInit {
 
   heroes: Hero[];
 
-  selectedHero: Hero;
-
   constructor(private heroService: HeroService) {
 
-   }
+  }
 
   ngOnInit() {
     this.getHeroes();
@@ -25,9 +23,4 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
   }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
 }
