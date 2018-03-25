f2610e058ca073d6be00d6e757828445feacbc37
Gregg Wartman <gregg.wartman@ontario.ca>
2018-03-24 23:13:45 -0400

    Update hero.service to return data using http

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
@@ -8,17 +9,25 @@ import { MessageService } from './message.service';
 @Injectable()
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes';  // URL to web api
+
+  constructor(private http: HttpClient,
+    private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
     this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    // return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
 
   getHero(id: number): Observable<Hero> {
     // Todo: send the message _after_ fetching the hero
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
+  }
+
+  private log(message: string) {
+    this.messageService.add('HeroService: ' + message);
   }
 
 }
